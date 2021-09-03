2a956ec3a51f7cd19b9428ad608586a7f6ae55a3
ilinaos <ilinaos@ya.ru>
2021-09-03 23:19:11 +0500

    Add files via upload

---
+++ homework_crowd.kt
@@ -0,0 +1,55 @@
+fun main() {
+    val colours: List<String> = listOf("Красный", "Черный", "Белый", "Серый", "Индиго")
+ val cars: List<String> = listOf("Lada", "Opel", "Skoda", "Volvo", "Toyota")
+ 
+ class Auto() {
+     val name: String = cars.get((0..4).random())
+     val colour: String = colours.get((0..4).random())
+     val tankCapacity: Int = (15..40).random()
+     val maxSpeed: Int = (120..334).random()
+     
+     fun info(){
+         println("$name")
+         println("$colour")
+         println("$tankCapacity")
+         println("$maxSpeed")
+     }
+ 
+ }
+ 
+ 
+ 
+ class Generator(){
+     val minCount: Int = (1..5).random()
+     val maxCount: Int = (minCount+1..minCount*2).random()
+     var crowd: MutableList<Auto> = mutableListOf()
+     
+     fun makeCars(){
+         val count: Int = (minCount..maxCount).random()
+         for (n in (1..count)){
+             crowd.add(Auto())
+         }
+ //         println("min - $minCount")
+ //         println("max - $maxCount")
+     }
+         
+      fun info2 (){
+         for (n in crowd){
+             n.info()
+             println("---------------")
+         }
+     }
+   
+     
+     
+ }
+ 
+ // var example = Auto ()
+ // example.info()
+ 
+ var example = Generator()
+ example.makeCars()
+ example.info2()
+ 
+ 
+ }
